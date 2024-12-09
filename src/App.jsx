59cb6e801b179b6e8f0aaf63cfdfb7661b2078a4
Takumasa Hirabayashi <26487896+himanago@users.noreply.github.com>
2024-12-09 15:06:56 +0000

    初期コード修正

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import liff from "@line/liff";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+
   useEffect(() => {
-    liff.init({ liffId: import.meta.env.VITE_LIFF_ID }).catch(console.error);
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID
+      })
+      .then(() => {
+        liff.getProfile()
+          .then((profile) => {
+            setName(profile.displayName);
+          })
+      })
   }, []);
   
   return (
     <div className="App">
-      {liff.id}
+      {name && <p>こんにちは、{name}さん</p>}
     </div>
   );
 }
